3a630c68df150579af1323efce583d460f96d250
arindam79 <54805141+arindam79@users.noreply.github.com>
2023-12-30 17:46:52 +0000

    added frontend

--- app.py
+++ app.py
@@ -1,11 +1,41 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+JOBS = [
+  {
+    'id': 1,
+    'title': 'Data Analyst',
+    'location': 'Bengaluru, India',
+    'salary': 'Rs. 1000000'
+  },
+  {
+    'id': 2,
+    'title': 'Data Scientist',
+    'location': 'Delhi, India',
+    'salary': 'Rs. 1500000'
+  },
+  {
+    'id': 3,
+    'title': 'Frontend Engineer',
+    'location': 'Remote',
+    
+  },
+  {
+    'id': 4,
+    'title': 'Backend Engineer',
+    'location': 'New York, USA',
+    'salary': '$120,000'
+  },
+]
 
 @app.route('/')
 def hello_world():
-  return "Hello World!"
+  return render_template('index.html', jobs=JOBS)
+
+@app.route('/jobs')
+def list_jobs():
+  return jsonify(JOBS)
 
 
 if __name__ == '__main__':
